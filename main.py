9d645f8055577da0eda3b3c93e7e9a8e2bf3d186
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-03-22 16:17:33 -0500

    Update main.py
    - no more flashing images
    - list of NPCs

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from characters.juno import *
 from characters.npc import *
-
 import pygame
-
-
 
 def main():
     pygame.init()
@@ -11,7 +8,7 @@ def main():
     clock = pygame.time.Clock()
     pygame.display.set_caption("Deep Murder (Juno / beta version)")
     j: Juno = Juno(50, 50, speed=10)
-    npc: NPC = NPC(400, 100) 
+    npcs: list[NPC] = [NPC(100 , 100) , NPC(300,300)]
     border = pygame.Rect(0, 0, win.get_width(), win.get_height())
     status: bool = True
     while status:
@@ -23,15 +20,17 @@ def main():
                 border = pygame.Rect(0, 0, win.get_width(), win.get_height())
         keys = pygame.key.get_pressed()
         j.move(keys, border)
-        npc.move()
-        draw_window(npc, border , win)
-        draw_window(j, border, win)
+        draw_window(j, npcs ,border , win)
+
 
     pygame.quit()
 
-def draw_window(sample_char, barrier , window):
+def draw_window(player, npcs: list[NPC] , barrier , window):
     window.fill((147, 204, 207))
-    sample_char.draw(window)
+    player.draw(window)
+    for npc in npcs:
+        npc.move()
+        npc.draw(window)
     pygame.draw.rect(window, (189 , 52 , 235), barrier, 10)
     pygame.display.update()
 
