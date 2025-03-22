89a762fc353819772c8ec7cc973549c1577b8f07
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2025-03-22 15:19:09 -0500

    Update juno.py
    - diagonal speed is the same as moving in one direction
    - border updates with resize

--- juno.py
+++ juno.py
@@ -1,26 +1,27 @@
 import pygame
-from pygame.rect import RectType
 
 pygame.init()
-screen = pygame.display.set_mode((1000 , 800) , pygame.RESIZABLE)
+window = pygame.display.set_mode((1000 , 800) , pygame.RESIZABLE)
 clock = pygame.time.Clock()
-pygame.display.set_caption("Juno :D")
+pygame.display.set_caption("Deep Murder (Juno / beta version)")
 
-def draw_window(sample_char , barrier):
-    screen.fill((147,204,207))
-    sample_char.draw(screen)
-    pygame.draw.rect(screen , (189 , 52 , 235) , barrier , 10)
+def draw_window(sample_char, barrier):
+    window.fill((147, 204, 207))
+    sample_char.draw(window)
+    pygame.draw.rect(window, (189 , 52 , 235), barrier, 10)
     pygame.display.update()
 
 def main():
-    j: Juno = Juno(100,100 , speed=15)
-    border = pygame.Rect(0, 0, screen.get_width(), screen.get_height())
+    j: Juno = Juno(50,50 , speed=10)
+    border = pygame.Rect(0, 0, window.get_width(), window.get_height())
     status: bool = True
     while status:
         clock.tick(60)
         for event in pygame.event.get():
-            if event.type is pygame.QUIT:
+            if event.type == pygame.QUIT:
                 status = False
+            elif event.type == pygame.VIDEORESIZE:
+                border = pygame.Rect(0, 0, window.get_width(), window.get_height())
         keys = pygame.key.get_pressed()
         j.move(keys , border)
         draw_window(j , border)
@@ -36,18 +37,25 @@ class Juno(object):
         self.speed = speed
 
     def move(self , keys , barrier) -> None:
-        bar_x , bar_y = self.rect.x , self.rect.y
+        dx , dy = 0 , 0
 
         if keys[pygame.K_a]:
-            bar_x -= self.speed
+            dx -= 1
         if keys[pygame.K_d]:
-            bar_x += self.speed
+            dx += 1
         if keys[pygame.K_w]:
-            bar_y -= self.speed
+            dy -= 1
         if keys[pygame.K_s]:
-            bar_y += self.speed
+            dy += 1
         if keys[pygame.K_q]:
             pygame.quit()
+
+        if dx is not 0 and dy is not 0:
+            dx *= 0.7071
+            dy *= 0.7071
+
+        bar_x = self.rect.x + dx * self.speed
+        bar_y = self.rect.y + dy * self.speed
 
         tmp_rect = pygame.Rect(bar_x , bar_y , self.rect.width , self.rect.height)
         if barrier.contains(tmp_rect):
