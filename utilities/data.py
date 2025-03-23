207caa525f5203e34764bcd3e0ef1699a788f8e8
IzZumi228 <75075390+IzZumi228@users.noreply.github.com>
2025-03-23 05:46:54 -0400

    fixes and improvements
    -added audio
    -added animation
    -npcs are located to according positions

--- utilities/data.py
+++ utilities/data.py
@@ -7,41 +7,49 @@ CHARACTERS = [
         "init_pos": (200, 650),
         "movable_area": (400, 150)
     },
+    {
+        "name": "Finnley 'Finn' Thatch",
+        "description": "A cheerful fisherman who loves animals and tells tall tales.",
+        "place": "lake",
+        "spriteDir": "finn",
+        "init_pos": (600, 1000),
+        "movable_area": (900, 1300)
+    },
+    {
+        "name": "Elsie Bloom",
+        "description": "A kind-hearted botanist passionate about growing exotic plants.",
+        "place": "field",
+        "spriteDir": "elsie",
+        "init_pos": (1500, 1050),
+        "movable_area": (300, 150)
+    },
+    {
+        "name": "Marlowe Reed",
+        "description": "A mysterious traveler who sells rare seeds and artifacts.",
+        "place": "tavern",
+        "spriteDir": "reed",
+        "init_pos": (1900, 600),
+        "movable_area": (-500, -200)
+    },
+    {
+        "name": "Garrick 'Garry' Ironwood",
+        "description": "A stern but loyal town guard who patrols the village on his trusty horse, Ember.",
+        "place": "entrance",
+        "spriteDir": "garry",
+        "init_pos": (860, 120),
+        "movable_area": (1360, 0)
+    } 
 
 ]
 
 
-        # {
-        # "name": "Finnley 'Finn' Thatch",
-        # "description": "A cheerful fisherman who loves animals and tells tall tales.",
-        # "place": "lake",
-        # "spriteDir": "finn",
-        # "init_pos": (200, 650)
-        # },
+        
 
-    # {
-    #     "name": "Marlowe Reed",
-    #     "description": "A mysterious traveler who sells rare seeds and artifacts.",
-    #     "place": "tavern",
-    #     "spriteDir": "reed",
-    #     "init_pos": (700, 700)
-    # },
-
-    # {
-    #     "name": "Elsie Bloom",
-    #     "description": "A kind-hearted botanist passionate about growing exotic plants.",
-    #     "place": "field",
-    #     "spriteDir": "elsie",
-    #     "init_pos": (700, 200)
-    # },
+    # 
 
 
-    # {
-    #     "name": "Garrick 'Garry' Ironwood",
-    #     "description": "A stern but loyal town guard who patrols the village on his trusty horse, Ember.",
-    #     "place": "entrance",
-    #     "spriteDir": "garry",
-    #     "init_pos": (560, 400)
-    # } 
+
+
+ 
 
 dialogues = []
